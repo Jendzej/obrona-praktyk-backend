659ad4acb6247808a447314b89594b7f188aea6c
Jędrzej <jedrzej@Mac-mini-Jedrzej.local>
2022-11-03 11:04:00 +0100

    Changes: Changing add function into two (single model and multiple models)

--- src/database.py
+++ src/database.py
@@ -1,13 +1,10 @@
 import os
+import time
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import Sequence, Column, Integer, String, Float, ForeignKey, DateTime, Table
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.exc import IntegrityError
-# from psycopg2 import errors
-import time
-import datetime
+from sqlalchemy.orm import sessionmaker
 
 from models import create_models
 
@@ -46,104 +43,20 @@ class Database:
         model_list = create_models(self.engine, base)
         return model_list
 
-    def add_item(self, item_model, item_name, item_price, item_description, item_image_url):
+    def add_data(self, model):
         session = sessionmaker(bind=self.engine)
         sess = session()
-        item_to_add = item_model(
-            item_name=item_name,
-            item_price=item_price,
-            item_description=item_description,
-            item_image_url=item_image_url
-        )
-        sess.add(item_to_add)
+        sess.add(model)
         sess.commit()
-        data = sess.query(item_model).first()
-        return data
 
-    def add_school_classes(self, school_class_model, school_class):
+    def add_multiple_data(self, models: list):
         session = sessionmaker(bind=self.engine)
         sess = session()
-        school_class_to_add = school_class_model(
-            school_class=school_class
-        )
-        sess.add(school_class_to_add)
 
-        try:
-            sess.commit()
-            data = sess.query(school_class_model).first()
-        except IntegrityError as ie:
-            return ie
+        for model in models:
+            sess.add(model)
 
-        return data
-
-    def add_role(self, role_model, role):
-        session = sessionmaker(bind=self.engine)
-        sess = session()
-        role_to_add = role_model(
-            role=role
-        )
-        sess.add(role_to_add)
-        try:
-            sess.commit()
-            data = sess.query(role_model).first()
-        except IntegrityError as ie:
-            return ie
-
-        return data
-
-    def add_payment_status(self, payment_status_model, status):
-            session = sessionmaker(bind=self.engine)
-            sess = session()
-            status_to_add = payment_status_model(
-                status=status
-            )
-            sess.add(status_to_add)
-            try:
-                sess.commit()
-                data = sess.query(payment_status_model).first()
-            except IntegrityError as ie:
-                return ie
-
-            return data
-
-    def add_user(self, user_model, username, first_name, last_name, password, role, school_class):
-        session = sessionmaker(bind=self.engine)
-        sess = session()
-        user_to_add = user_model(
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-            password=password,
-            role=role,
-            school_class=school_class
-        )
-        sess.add(user_to_add)
-
-        try:
-            sess.commit()
-            data = sess.query(user_model).first()
-        except IntegrityError as ie:
-            return ie
-
-        return data
-
-    def add_transaction(self, transaction_model, user, item, payment_status):
-        session = sessionmaker(bind=self.engine)
-        sess = session()
-        transaction_to_add = transaction_model(
-            user=user,
-            item=item,
-            payment_status=payment_status
-        )
-        sess.add(transaction_to_add)
-
-        try:
-            sess.commit()
-            data = sess.query(transaction_model).first()
-        except IntegrityError as ie:
-            return ie
-
-        return data
+        sess.commit()
 
 
 load_dotenv()
@@ -166,14 +79,19 @@ users_model = models[3]
 transactions_model = models[4]
 payment_status_model = models[5]
 
-# print(models)
-# print(item_model)
+# print(db.add_data(school_class_models(school_class="4TIP")))
+# print(db.add_data(roles(role="admin")))
 
-# print(db.add_role(roles, "admin"))
-# print(db.add_school_classes(school_class_models, "4TIP"))
-# print(db.add_payment_status(payment_status_model, "paid"))
-# print(db.add_payment_status(payment_status_model, "not paid"))
-# print(db.add_payment_status(payment_status_model, "pending"))
-# print(db.add_item(item_model, "Bułka", 10.10, "Bułka smaczna, z ogórkiem", "url"))
-# print(db.add_user(users_model, "jendrzej24", "Jędrzej", "Runowicz", "Haslo", "admin", "4TIP"))
-print(db.add_transaction(transactions_model, "jendrzej24",  "Bułka", "paid"))
+# print(item_model)
+# print(db.add_data(
+#     users_model(username="jedrzej2115", first_name="jedrzej", last_name="runowicz", password="haslo",
+#                 role="admin",
+#                 school_class="4TIP")))
+
+# db.add_multiple_data([
+#     item_model(item_name="Bułka", item_price=14.23, item_description="Opis bułki", item_image_url="url"),
+#     item_model(item_name="Bagietka", item_price=4.23, item_description="Opis bagietki", item_image_url="url")])
+
+# db.add_data(payment_status_model(status="paid"))
+
+# db.add_data(transactions_model(user="jedrzej2115", item="Bagietka", payment_status="paid"))
