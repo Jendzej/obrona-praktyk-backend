a4ab217c709efc119ebaba9e08cb6766a4813740
Jędrzej Runowicz <jedrzej.runowicz@gmail.com>
2022-11-07 20:35:13 +0100

    Changes: Moving some functions (main.py -> auth.py)

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-import datetime
 import os
 
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import FastAPI, Response
+from fastapi import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer
 from jose import jwt
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 
 from src.database import Database
+from src.routers.auth import router as AuthRouter
 
 load_dotenv()
 Base = declarative_base()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 app = FastAPI()
 app.add_middleware(
@@ -22,6 +30,7 @@ app.add_middleware(
     allow_methods=["POST", "GET"],
     allow_headers=["*"]
 )
+app.include_router(AuthRouter)
 
 # CONNECTING TO DB
 db = Database(os.getenv("POSTGRES_USER"), os.getenv("POSTGRES_PASSWORD"), os.getenv("POSTGRES_HOST"),
@@ -40,44 +49,17 @@ db.delete.item(model_of_item, 'item1')
 db.delete.user(model_of_user, 'user1')
 
 
-@app.post('/user')
-async def user(body: dict):
-    """Endpoint for adding user"""
-    hashed_password = jwt.encode({body['username']: body['password']}, os.getenv('SECRET_KEY'), os.getenv('ALGORITHM'))
-    role = 'user'
-    try:
-        db.insert.user(model_of_user, body['username'], body['email'], body['first_name'], body['last_name'],
-                       hashed_password, role, body['school_class'])
-    except IntegrityError:
-        raise HTTPException(
-            status_code=401,
-            detail="Email or username are already taken"
-        )
-    return Response(status_code=200, content='OK')
-
-
-@app.post('/add_transaction')
-async def add_item_to_transaction(body: dict):
-    # user, items: list, payment_status
-    user: str = body['user']
-    items: list = body['items']
-    payment_status: str = body['payment_status']
-    transaction_time: datetime = datetime.datetime.today()
-    for item in items:
-        db.insert.transaction(model_of_transaction(user=user, item=item, payment_status=payment_status,
-                                                   transaction_time=transaction_time))
-    db.insert.group_transaction(model_of_transaction, model_of_gr_transaction, model_of_item, user, transaction_time)
-
-
 @app.post('/users')
 async def asks(body: dict):
     """Endpoint for adding users"""
     password = body['password']
     username = body['username']
-    mail = body['mail']
+    email = body['mail']
     hashed_password = jwt.encode({username: password}, SECRET_KEY, ALGORITHM)
+    role = 'user'
     try:
-        create_session_user(engine, UserModel, mail, hashed_password, username)
+        db.insert.user(model_of_user, username, email, body['first_name'], body['last_name'], hashed_password, role,
+                       body['school_class'])
     except IntegrityError:
         print("Email or username is already taken")
         raise HTTPException(
@@ -85,101 +67,6 @@ async def asks(body: dict):
             detail="Email or username is already taken"
         )
     return Response(status_code=200, content="OK")
-
-
-""" Authorization part of backend """
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def verify_password(username, plain_password, hashed_password):
-    decoded_hashed = jwt.decode(hashed_password, SECRET_KEY, ALGORITHM)
-    if decoded_hashed[username] == plain_password:
-        return True
-    else:
-        return False
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def get_user(username: str):
-    users = engine.execute(f"SELECT username, mail, hashed_password FROM users WHERE username = '{username}'")
-    results = users.fetchall()
-    try:
-        user = results[0]
-    except IndexError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="This username does not exist",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    # user_model = User(username=user[0], mail=user[1])
-    return UserInDb(username=user[0], mail=user[1], hashed_password=user[2])
-
-
-def authenticate_user(username: str, password: str):
-    user = get_user(username)
-    if not user:
-        return False
-    if not verify_password(username, password, user.hashed_password):
-        return False
-    return user
-
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    return current_user
-
-
-@app.post("/token", response_model=Token)
-async def login_for_access_token(
-        form_data: OAuth2PasswordRequestForm = Depends()):  # form_data:OAuth2PasswordRequestForm = Depends()
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    access_token_expires = timedelta(minutes=TOKEN_EXPIRE)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
 
 
 if __name__ == "__main__":
--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,120 +1,121 @@
 import os
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
 
-from fastapi import HTTPException, status, Depends, APIRouter
+from dotenv import load_dotenv
+from fastapi import HTTPException, Depends, status, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
 from passlib.context import CryptContext
-from pydantic import BaseModel
 from sqlalchemy.exc import NoResultFound
+from sqlalchemy.ext.declarative import declarative_base
 
-from src.data_functions.data_fetch import Fetch
+from src.database import Database
+from src.models import TokenData, User, UserInDb, Token
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 router = APIRouter(
-    tags=['Auth']
+    tags=['auth']
 )
 
+Base = declarative_base()
+db = Database(os.getenv("POSTGRES_USER"), os.getenv("POSTGRES_PASSWORD"), os.getenv("POSTGRES_HOST"),
+              os.getenv("POSTGRES_PORT"), os.getenv("POSTGRES_DB"))
+db.connecting_db()
 
-class Authorization:
-    def __init__(self, engine, user_model):
-        self.secret_key = os.getenv("SECRET_KEY")
-        self.algorithm = os.getenv("ALGORITHM")
-        self.token_expire = float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-        self.engine = engine
-        self.fetch = Fetch(self.engine)
-        self.user_model = user_model
+model_list = db.init.create_tables(Base)
+model_of_user = model_list[1]
 
-    class Token(BaseModel):
-        access_token: str
-        token_type: str
 
-    class TokenData(BaseModel):
-        username: str | None = None
+def verify_password(username, plain_password, hashed_password):
+    decoded_hashed = jwt.decode(hashed_password, SECRET_KEY, ALGORITHM)
+    if decoded_hashed[username] == plain_password:
+        return True
+    else:
+        return False
 
-    class User(BaseModel):
-        username: str
-        mail: str
 
-    class UserInDb(User):
-        hashed_password: str
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-    def verify_password(self, username, plain_password, hashed_password):
-        decoded_hashed = jwt.decode(hashed_password, self.secret_key, self.algorithm)
-        if decoded_hashed[username] == plain_password:
-            return True
-        else:
-            return False
 
-    def get_password_hash(self, password):
-        return self.pwd_context.hash(password)
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
-    def get_user(self, username: str):
-        try:
-            user = self.fetch.user(self.user_model, username)
-        except NoResultFound:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='This username does not exist',
-                headers={'WWW-Authenticate': "Bearer"}
-            )
-        return self.UserInDb(username=user.username, mail=user.mail, hashed_password=user.password)
 
-    def authenticate_user(self, username: str, password: str):
-        user = self.get_user(username)
-        if not user:
-            return False
-        if not self.verify_password(username, password, user.hashed_password):
-            return False
-        return user
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
 
-    def create_access_token(self, data: dict, expires_delta: timedelta | None = None):
-        to_encode = data.copy()
-        if expires_delta:
-            expire = datetime.utcnow() + expires_delta
-        else:
-            expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
-        to_encode.update({"exp": expire})
-        encoded_jwt = jwt.encode(to_encode, self.secret_key, algorithm=self.algorithm)
-        return encoded_jwt
 
-    oauth_scheme = OAuth2PasswordBearer(tokenUrl="/token")
-
-    async def get_current_user(self, token: str = Depends(oauth_scheme)):
-        credentials_exception = HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
+def get_user(username: str):
+    try:
+        user = db.fetch.user(model_of_user, username)
+    except NoResultFound:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="This username does not exist",
             headers={"WWW-Authenticate": "Bearer"}
         )
-        try:
-            payload = jwt.decode(token, self.secret_key, algorithms=[self.algorithm])
-            username: str = payload.get("sub")
-            if username is None:
-                raise credentials_exception
-            token_data = self.TokenData(username=username)
-        except JWTError:
-            raise credentials_exception
-        user = self.get_user(username=token_data.username)
-        if user is None:
-            raise credentials_exception
-        return user
+    return UserInDb(username=user.username, mail=user.email, hashed_password=user.password)
 
-    @staticmethod
-    async def get_current_active_user(current_user: User = Depends(get_current_user)):
-        return current_user
 
-    @router.post("/token", response_model=Token)
-    async def login_for_access_token(self, form_data: OAuth2PasswordRequestForm = Depends()):
-        user = self.authenticate_user(form_data.username, form_data.password)
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Incorrect username or password",
-                headers={"WWW-Authenticate": "Bearer"}
-            )
-        access_token_expires = timedelta(minutes=self.token_expire)
-        access_token = self.create_access_token(
-            data={"sub": user.username}, expires_delta=access_token_expires
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(username, password, user.hashed_password):
+        return False
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    return current_user
+
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+        form_data: OAuth2PasswordRequestForm = Depends()):  # form_data:OAuth2PasswordRequestForm = Depends()
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
         )
-        return {"access_token": access_token, "token_type": "bearer"}
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
