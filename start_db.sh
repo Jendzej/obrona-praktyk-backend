97d852e577526c74def28062645b287dec60f352
Jędrzej Runowicz <jedrzej.runowicz@gmail.com>
2022-11-09 18:52:34 +0100

    Fixes: bash file fix

--- start_db.sh
+++ start_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d --env-file ./.env -p 5432:5432 -p 15432:15432 --name backend_db postgres
+docker run --env-file ./.env -p 5432:5432 -p 15432:15432 --name -d backend_db postgres
 sleep 5
 docker cp ./conf/pg_hba.conf backend_db:/var/lib/postgresql/data/
 docker restart backend_db
