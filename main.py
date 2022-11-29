7c4ad53f5062c5c678c1d4556538400ea8682bad
Jędrzej Runowicz <jedrzej.runowicz@gmail.com>
2022-11-29 15:18:51 +0100

    Heroku fix

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 """Database initiation, models (tables) creation, run fastapi server on `python run`"""
 import json
+import os
 
+import uvicorn
+from dotenv import load_dotenv
 from sqlalchemy.ext.declarative import declarative_base
 
 from src.db import db
+
+load_dotenv()
 
 Base = declarative_base()
 db.connecting_db()
@@ -14,3 +19,11 @@ other_models = db.models[0]
 with open('startup_data.json', 'r', encoding='UTF-8') as startup_data_file:
     startup_data = json.load(startup_data_file)
     startup_class, startup_roles, startup_status, startup_items = startup_data.values()
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "api:app",
+        host="0.0.0.0",
+        reload=True,
+        port=os.getenv("PORT")
+    )
